16749c51686a896bd5c21e450b2ad676aefa4568
ek9 <code@ek9.co>
2016-12-31 03:51:22 +0100

    css/html grid resize + add navigation/hamburger menu (with JS) + move css to style.css + add scripts.js + remove <=IE8 support

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+/**
+ * ek9.co scripts.js
+ * author: ek9 <dev@ek9.co> 2016
+ */
+
+/**
+ * navigation & hamburger menu toggle script
+ */
+(function (window, document) {
+
+    var menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink'),
+        content  = document.getElementById('main');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    function toggleAll(e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    }
+
+    menuLink.onclick = function (e) {
+        toggleAll(e);
+    };
+
+    content.onclick = function(e) {
+        if (menu.className.indexOf('active') !== -1) {
+            toggleAll(e);
+        }
+    };
+
+}(this, this.document));
